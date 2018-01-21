82d83dd47d3b3c1c3aa790afcf517056d83bc077
marek <mlyczba@gmail.com>
2018-01-21 16:44:18 +0100

    Changes in seed servers.

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,8 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "cets-seed-1.visualshapers.com:8090",
-  "cets-seed-2.visualshapers.com:8090",
+  "seed1.cetscoin.eu:8090",
+  "seed2.cetscoin.eu:8090",
 };
 
 struct CheckpointData {
